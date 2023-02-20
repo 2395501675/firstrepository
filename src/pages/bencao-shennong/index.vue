399f1ba18d1a39384e4983f9b3aec7fe997d8602
李宁宁 <ningning.li@medsci.cn>
2023-02-20 15:38:54 +0800

    11
    Signed-off-by: 李宁宁 <ningning.li@medsci.cn>

--- src/pages/bencao-shennong/index.vue
+++ src/pages/bencao-shennong/index.vue
@@ -29,6 +29,7 @@
               v-else
               :index="i + '-' + childIndex"
               :key="child.title"
+              @click="(e) =>showyao(child,e)"
             >
               {{ child.title }}
             </el-menu-item>
@@ -36,8 +37,8 @@
         </el-sub-menu>
       </el-menu>
     </div>
-    <div style="width:100%">
-      <el-table :data="zongyi.wuxing" style="width: 100%">
+    <div style="width:100%" >
+      <el-table :data="zongyi.wuxing" v-if="danweiyao.length ===0" style="width: 100%" >
         <el-table-column prop="weiyi" label="味义" />
         <el-table-column prop="seyi" label="色义" />
         <el-table-column prop="weiyong" label="五味之用" />
@@ -48,6 +49,31 @@
         <el-table-column prop="bingjin" label="五病所禁" /> 
         <el-table-column prop="suoshang" label="五病所伤" /> 
       </el-table>
+
+      <el-input v-model="search" size="large" placeholder="Type to search" />
+      <el-table :data="filterTableData" height="800" style="width: 100%" :row-class-name="tableRowClassName">
+        <el-table-column prop="ming" label="药名"
+        :filter-method="filterTag"
+        :filters="[
+        { text: '常用', value: 1 },
+      ]">
+         <template #default="scope">
+        <el-tag
+          :type="scope.row.changyong === 1 ? 'success' : 'info'"
+          disable-transitions
+          >{{ scope.row.ming }}</el-tag
+        >
+      </template>
+      </el-table-column>
+        <el-table-column prop="xingweiguijing" label="性味归经" />
+        <el-table-column prop="gongneng" label="功能主治" />
+        <el-table-column prop="tedian" label="特点" />
+        <el-table-column prop="jinji" label="禁忌" />
+        <el-table-column prop="paozhi" label="炮制" />
+        <el-table-column prop="fangji" label="方剂" />
+        <el-table-column prop="qita" label="其他" />
+       
+      </el-table>
     </div>
 </div>
    
@@ -57,7 +83,35 @@ import {
   menuList,
   zongyi
 } from './data.js'
+import{ref,computed} from 'vue'
+const search = ref('')
+const filterTag = (value, row) => {
+  return row.changyong === value
+}
 
+const tableRowClassName = ({
+  row,
+  rowIndex,
+}) => {
+  console.log(111,row)
+  // if (row.changyong === 1) {
+  //   return 'success-row'
+  // }
+  return ''
+}
+const danweiyao = ref([])
+function showyao(item) {
+  danweiyao.value = item.danweiyao
+  console.log(1111,item,danweiyao)
+}
+
+const filterTableData = computed(() =>
+  danweiyao.value.filter(
+    (data) =>
+      !search.value ||
+      data.ming.toLowerCase().includes(search.value.toLowerCase())
+  )
+)
 </script>
 <style>
 .demo-tabs > .el-tabs__content {
@@ -66,5 +120,11 @@ import {
 ul,
 li {
   list-style-type: none;
+}
+.el-table .warning-row {
+  --el-table-tr-bg-color: var(--el-color-warning-light-9);
+}
+.el-table .success-row {
+  --el-table-tr-bg-color: var(--el-color-success-light-9);
 }
 </style>
